3019297be9d4aed4a85dca472e0faff2787d37b0
sohailamiransari <amirsohailansari89@gmail.com>
2024-08-13 15:13:08 +0530

    const let var

---
+++ scope.js
@@ -0,0 +1,16 @@
+const accountId = 12324;
+let accountEmail = "abc@gmail.com";
+var accountCity = "Banglore";
+let accountState;
+
+// console.log(accountId);
+
+// accountId = 2314; //error because we can't change variable when we declare const variable. 
+// console.log(accountId);
+
+// if we want to print table then 
+console.table([accountId, accountEmail, accountCity, accountState]);
+
+// console.log(accountState) // accountState is undefine because we have declare the value but not fill the value.
+
+
